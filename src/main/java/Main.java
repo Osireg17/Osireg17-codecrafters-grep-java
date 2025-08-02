181603df0749615361ab10589a556375cb573148
Osi Obomighie <93836683+Osireg17@users.noreply.github.com>
2025-08-02 19:23:58 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,35 @@
-import java.io.IOException;
+
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args){
-    if (args.length != 2 || !args[0].equals("-E")) {
-      System.out.println("Usage: ./your_program.sh -E <pattern>");
-      System.exit(1);
+
+    public static void main(String[] args) {
+        if (args.length != 2 || !args[0].equals("-E")) {
+            System.out.println("Usage: ./your_program.sh -E <pattern>");
+            System.exit(1);
+        }
+
+        String pattern = args[1];
+        Scanner scanner = new Scanner(System.in);
+        String inputLine = scanner.nextLine();
+
+        System.err.println("Logs from your program will appear here!");
+
+        if (matchPattern(inputLine, pattern)) {
+            System.exit(0);
+        } else {
+            System.exit(1);
+        }
     }
 
-    String pattern = args[1];  
-    Scanner scanner = new Scanner(System.in);
-    String inputLine = scanner.nextLine();
-
-    
-    System.err.println("Logs from your program will appear here!");
-
-
-    
-     if (matchPattern(inputLine, pattern)) {
-         System.exit(0);
-     } else {
-        System.exit(1);
-     }
-  }
-
-  public static boolean matchPattern(String inputLine, String pattern) {
-    if (pattern.length() == 1) {
-      return inputLine.contains(pattern);
-    } else {
-      throw new RuntimeException("Unhandled pattern: " + pattern);
+    public static boolean matchPattern(String inputLine, String pattern) {
+        if (pattern.length() == 1) {
+            return inputLine.contains(pattern);
+        } else if (pattern.equals("\\d")) {
+            //if a digit is present return 0, if not return 1
+            return inputLine.matches(".*\\d.*");
+        } else {
+            throw new RuntimeException("Unhandled pattern: " + pattern);
+        }
     }
-  }
 }
